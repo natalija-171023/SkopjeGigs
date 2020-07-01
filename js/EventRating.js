9863865c65ffcaa5189e5d4a0e394b0c84209cd4
Natalija Vitanova <natalijavitanova@gmail.com>
2020-07-01 16:25:01 +0200

    Added commenting feature

--- js/EventRating.js
+++ js/EventRating.js
@@ -1,4 +1,7 @@
+var stars = 0;
+
 function rate(broj){
+    stars = broj;
     i=1;
     while(i<=broj)
     {
@@ -12,4 +15,41 @@ function rate(broj){
         n.innerHTML="&star;";
         i++;
     }
+
+}
+
+function addComment(){
+
+    var comment = document.getElementById("comment").value;
+    var ime = document.getElementById("ime").value;
+    var txt = "<img src=\"images/user2.png\" style=\"width: 50px; height: 50px;\" class=\"rounded-circle mr-2\">\n" +
+        "                            <div class=\"media-body\">\n" +
+        "                                <h4 ><span id='myIme'></span> <span id='myStars' class=\"text-warning\" style=\"font-size: small\"></span></h4>\n" +
+        "                                <p id='myComment'>Can't wait!</p>\n" +
+        "                            </div>\n" +
+        "                        </div>";
+    // Create a new element
+
+    var newLine = document.createElement("hr")
+    var newNode = document.createElement('div');
+    newNode.classList.add("media", "kom");
+    newNode.innerHTML = txt;
+
+
+// Get the reference node
+    var referenceNode = document.querySelector('.kom');
+    referenceNode.parentNode.insertBefore(newNode, referenceNode);
+    referenceNode.parentNode.insertBefore(newLine, referenceNode);
+    document.getElementById("myIme").innerText = ime;
+    document.getElementById("myComment").innerText = comment;
+    var stars1;
+    switch (stars){
+        case 1 : stars1 ='&starf;&star;&star;&star;&star;'; break;
+        case 2 : stars1 ='&starf;&starf;&star;&star;&star;'; break;
+        case 3 : stars1 ='&starf;&starf;&starf;&star;&star;'; break;
+        case 4 : stars1 ='&starf;&starf;&starf;&starf;&star;'; break;
+        case 5 : stars1 ='&starf;&starf;&starf;&starf;&starf;'; break;
+    }
+    document.getElementById("myStars").innerHTML = stars1;
+
 }
